888b13c45d8cd00e6be82905cdfc76e334587156
Chinmay Kumbhare <thechinmaykumbhare@gmail.com>
2021-09-21 14:58:05 +0530

    Added: Source Code + Problem Statement
    Handled a few bugs as well.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Calculator-21Sept-Assignment
+# Problem Statement:
+# ----------------------------------------------------------------------------
+1. Create a UI for calculator 
+2. Add button to "Clear" - which will restart calculation
+3. Ensure it perform mathematic operation
+	Addition operator
+ 	Subtraction operator
+ 	Multiplication operator
+ 	Division operator
+ 	And Exponent (Power) operator
+4. Show history of operation performed on right.
+# ----------------------------------------------------------------------------
---
+++ index.js
@@ -0,0 +1,64 @@
+let display = document.getElementById("display");
+buttons = document.querySelectorAll("button");
+let displayValue = "";
+
+let history = [];
+let log = document.getElementById("displayHistory");
+
+//get the button that is clicked
+for(btn of buttons) {
+    //console.log(btn);
+    //btn = btn.innerText;
+    //console.log(btn);
+
+    //get the button that is clicked
+    btn.addEventListener('click', (b) => {
+        //get text
+        buttonVal = b.target.innerText;
+        // console.log(buttonVal);
+
+        //displayValue for operating
+        //displayValue += buttonVal;
+
+        //display text to textbox
+        //display.value = displayValue;
+
+        if(buttonVal === "^") buttonVal = "**";
+
+        //clear
+        if(buttonVal === "C") {
+            display.value = "";
+            displayValue = "";
+        } else if(buttonVal === "=") {
+            display.value = eval(displayValue);
+
+            history.push(displayValue +"=" +display.value);
+            // console.log(history);
+
+            displayValue = "";
+            //display.value = "";
+
+            // log.innerHTML = "<ul>" +history +"<ul>";
+            
+
+        } else if(buttonVal === "LOGS") {
+            let temp = document.getElementById("displayHistory");
+            temp.remove();
+            let ul = document.createElement("ul");
+            ul.id = "displayHistory";
+
+            history.forEach(element => {
+                let li = document.createElement("li");
+                li.innerText = element;
+                ul.appendChild(li);
+            });
+
+            document.body.appendChild(ul);
+        } else {
+            displayValue += buttonVal;
+            display.value = displayValue;
+        }
+
+        // console.log(displayValue);
+    })
+}
